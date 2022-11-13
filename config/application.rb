f2df27658bdcac92dc4e0ce2c800f3b30ae5cd0c
Cherry Ramatis <cherry.ramatis@gmail.com>
2022-11-13 19:36:08 -0300

    Configure i18n

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Planerino
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = :pt
 
     # Configuration for the application, engines, and railties goes here.
     #
