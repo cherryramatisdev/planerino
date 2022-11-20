819961ca23ccc3087b1218d611d1bc808f9a19a8
Cherry Ramatis <cherry.ramatis@gmail.com>
2022-11-20 15:26:42 -0300

    Add debit total to month list

--- app/models/debit.rb
+++ app/models/debit.rb
@@ -1,20 +1,20 @@
 class Debit < ApplicationRecord
   before_save :transform_title
 
-  validates :title, :presence => {:message => " nao pode ser vazio"}
-  validates :price, :presence => {:message => " nao pode ser vazio"}
-  validates :owner, :presence => {:message => "Precisa informar dono"}
+  validates :title, presence: { message: ' nao pode ser vazio' }
+  validates :price, presence: { message: ' nao pode ser vazio' }
+  validates :owner, presence: { message: 'Precisa informar dono' }
 
   belongs_to :owner
   belongs_to :month
 
   def self.toggle_paid(id)
-    debit = self.find(id)
-    
-    self.update(debit.id, paid: !debit.paid)
+    debit = find(id)
+
+    update(debit.id, paid: !debit.paid)
   end
 
   def transform_title
-    self.title.upcase!
+    title.upcase!
   end
 end
--- app/models/month.rb
+++ app/models/month.rb
@@ -1,5 +1,30 @@
 class Month < ApplicationRecord
-  validates :name, :presence => {:message => " nao pode ser vazio"}
+  validates :name, presence: { message: ' nao pode ser vazio' }
 
   has_many :debit, dependent: :destroy
+
+  def total
+    debits_not_paid.map(&:price).compact.inject(:+)
+  end
+
+  def total_per_owner
+    debits_per_owner = []
+
+    debits_not_paid.group_by(&:owner_id).each do |key, value|
+      debits_per_owner << {
+        owner: Owner.find(key).name,
+        total: value.map(&:price).inject(:+)
+      }
+    end
+
+    debits_per_owner
+  end
+
+  private
+
+  def debits_not_paid
+    debits_not_paid = debit.map { |d| d unless d.paid }
+
+    debits_not_paid.compact
+  end
 end
