bb1f2ceea0d3f25ca83bd1b3180dac9fea7d36e7
Cherry Ramatis <cherry.ramatis@gmail.com>
2022-11-25 18:11:09 -0300

    Migrate from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_14_011918) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "debits", force: :cascade do |t|
     t.string "title"
     t.float "price"
     t.boolean "paid"
-    t.integer "owner_id", null: false
-    t.integer "month_id", null: false
+    t.bigint "owner_id", null: false
+    t.bigint "month_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["month_id"], name: "index_debits_on_month_id"
