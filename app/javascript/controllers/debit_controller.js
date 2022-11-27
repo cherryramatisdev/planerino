a959248f5312fa25fe8112b9e67c9f217c605980
Cherry Ramatis <cherry.ramatis@gmail.com>
2022-11-27 16:56:44 -0300

    Add interactively on click

--- app/javascript/controllers/debit_controller.js
+++ app/javascript/controllers/debit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["clickarea", "arrowicon", "payable"]
+  static targets = ["clickarea", "arrowicon", "payable", "ownertotal"]
 
   hideElement(element) {
     element.classList.remove('flex')
@@ -44,9 +44,39 @@ this.clickareaTarget.classList.add("mb-2")
     return siblings;
   }
 
+  async updateTotalDebitForOwner(owner_id) {
+    const response = await fetch(`/owner/get_debit_total/${owner_id}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }})
+
+    if (response.status === 200) {
+      const { total } = await response.json()
+
+      document.getElementById(`total_owner-${owner_id}`).textContent = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(total)
+    }
+  }
+
+  async updateTotalForDebit(debit_title) {
+    const response = await fetch(`/debit/get_debit_total?title=${debit_title}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }})
+
+    if (response.status === 200) {
+      const { total } = await response.json()
+
+      document.getElementById(`total_debit-${debit_title}`).textContent = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(total)
+    }
+  }
+
   // ACTIONS
 
-  toggle() {
+  toggle(event) {
+    event.preventDefault();
+
     let siblings = this.siblings(this.clickareaTarget)
 
     for (let sibling of siblings) {
@@ -66,20 +96,28 @@ this.clickareaTarget.classList.add("mb-2")
     const body = JSON.parse(this.payableTarget.dataset.info)
     const url = `/debits/update_paid/${body.id}?month_id=${body.month_id}&id=${body.id}`
 
-    const { debit_paid } = await fetch(url, {
+    const response = await fetch(url, {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-      }}).then(r => r.json())
+      }})
 
-    if (debit_paid) {
-      this.payableTarget.childNodes[3].classList.remove("bg-yellow-300")
-      this.payableTarget.childNodes[3].classList.add("bg-green-300")
-      this.payableTarget.childNodes[3].textContent = "Pago"
-    } else {
-      this.payableTarget.childNodes[3].classList.remove("bg-green-300")
-      this.payableTarget.childNodes[3].classList.add("bg-yellow-300")
-      this.payableTarget.childNodes[3].textContent = "Pendente"
+
+    if (response.status === 200) {
+      const { debit_paid } = await response.json()
+
+      await this.updateTotalDebitForOwner(body.owner_id)
+      await this.updateTotalForDebit(body.debit_title)
+
+      if (debit_paid) {
+        this.payableTarget.childNodes[3].classList.remove("bg-yellow-300")
+        this.payableTarget.childNodes[3].classList.add("bg-green-300")
+        this.payableTarget.childNodes[3].textContent = "Pago"
+      } else {
+        this.payableTarget.childNodes[3].classList.remove("bg-green-300")
+        this.payableTarget.childNodes[3].classList.add("bg-yellow-300")
+        this.payableTarget.childNodes[3].textContent = "Pendente"
+      }
     }
   }
 }
