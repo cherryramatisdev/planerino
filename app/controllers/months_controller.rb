a203c63aa5ea66c6dde19c22283121e04e769af4
Cherry Ramatis <cherry.ramatis@gmail.com>
2022-11-28 16:51:15 -0300

    Send user to sign in if not authenticated

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -35,13 +35,17 @@ class MonthsController < ApplicationController
 
   # POST /months or /months.json
   def create
-    @month = Month.new(month_params)
-
     respond_to do |format|
-      if @month.save
-        format.html { redirect_to months_path, notice: 'Mês foi criado com sucesso' }
+      if current_user.nil?
+        format.html { redirect_to new_user_session_path }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        @month = Month.new({}.merge(month_params, {user_id: current_user.id}))
+
+        if @month.save
+          format.html { redirect_to months_path, notice: 'Mês foi criado com sucesso' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
       end
     end
   end
