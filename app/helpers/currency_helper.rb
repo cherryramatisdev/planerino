9a092a2b3fe6e506c5bafb75ed72952157dedce7
Cherry Ramatis <cherry.ramatis@gmail.com>
2023-01-05 20:24:37 -0300

    Type helpers

--- app/helpers/currency_helper.rb
+++ app/helpers/currency_helper.rb
@@ -1,9 +1,16 @@
+# typed: true
+
 module CurrencyHelper
+  extend T::Sig
+  include ActionView::Helpers::NumberHelper
+
+  sig { params(amount: Integer).returns(String) }
   def number_to_brl(amount)
-    number_to_currency(amount, unit: 'R$ ', delimiter: '.', separator: ',')
+    number_to_currency(amount, unit: 'R$ ', delimiter: '.', separator: ',') || ''
   end
 
+  sig { params(amount: Integer).returns(String) }
   def number_to_string_with_delimiter(amount)
-    number_with_delimiter(amount, unit: '', delimiter: '.', separator: ',')
+    number_with_delimiter(amount, unit: '', delimiter: '.', separator: ',') || ''
   end
 end
