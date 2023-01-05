b937992aed54e1bbcf9b0163635dfd54d6e6b382
Cherry Ramatis <cherry.ramatis@gmail.com>
2023-01-05 20:47:59 -0300

    Accept possible null parameters

--- app/helpers/currency_helper.rb
+++ app/helpers/currency_helper.rb
@@ -4,12 +4,12 @@ module CurrencyHelper
   extend T::Sig
   include ActionView::Helpers::NumberHelper
 
-  sig { params(amount: Integer).returns(String) }
+  sig { params(amount: T.any(Float, Integer)).returns(String) }
   def number_to_brl(amount)
     number_to_currency(amount, unit: 'R$ ', delimiter: '.', separator: ',') || ''
   end
 
-  sig { params(amount: Integer).returns(String) }
+  sig { params(amount: T.nilable(T.any(Float, Integer))).returns(String) }
   def number_to_string_with_delimiter(amount)
     number_with_delimiter(amount, unit: '', delimiter: '.', separator: ',') || ''
   end
