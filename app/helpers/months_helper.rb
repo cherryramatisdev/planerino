522400ce3cf7788255277c68acd80d831418ef17
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-02 17:56:13 -0300

    Add sorting

--- app/helpers/months_helper.rb
+++ app/helpers/months_helper.rb
@@ -4,9 +4,6 @@ module MonthsHelper
   # @param first [Month]
   # @param second [Month]
   def self.sort_by_name(first, second)
-    regexp = /,|-|./
-    first_name = first.name.gsub(regexp, '')
-    second_name = second.name.gsub(regexp, '')
-    Month::MONTH_NAMES[first_name.split[0]] <=> Month::MONTH_NAMES[second_name.split[0]]
+    Month::MONTH_NAMES[first.name.split[0]] <=> Month::MONTH_NAMES[second.name.split[0]]
   end
 end
